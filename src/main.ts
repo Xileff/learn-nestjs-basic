47c3f6cb5022bf3dc35d10d42e886c846201fbe0
Felix <87980522+Xileff@users.noreply.github.com>
2024-06-04 12:33:08 +0700

    feat: logger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import * as cookieParser from 'cookie-parser';
 import * as mustache from 'mustache-express';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
 
   app.use(cookieParser('RAHASIA'));
   app.set('views', __dirname + '/../views');
--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class UserRepository {
-  constructor(private prismaService: PrismaService) {
-    console.info('Create user repository');
+  constructor(
+    private prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: Logger,
+  ) {
+    this.logger.log('Create user repository');
   }
 
   async save(firstName: string, lastName?: string): Promise<User> {
+    this.logger.debug(
+      `Create user with first name '${firstName}' and last name '${lastName}'`,
+    );
     return await this.prismaService.user.create({
       data: {
         first_name: firstName,
